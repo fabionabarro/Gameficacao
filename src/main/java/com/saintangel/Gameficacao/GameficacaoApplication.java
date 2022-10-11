fc96306352c7782a691e5efa5b5d1ef8169672f1
fabio.teixeira <fabteixeira@squaretrade.com>
2022-10-11 18:59:26 -0300

    Fabio - First commit

---
+++ src/main/java/com/saintangel/Gameficacao/GameficacaoApplication.java
@@ -0,0 +1,13 @@
+package com.saintangel.Gameficacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameficacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameficacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saintangel/Gameficacao/GameficacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saintangel.Gameficacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameficacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
